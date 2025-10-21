222cf0666c9fc74af3c767a1767b2af660bd4127
Đỗ Gia <giado280904@gmail.com>
2025-10-21 11:57:48 +0700

    chia layout

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,41 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('restaurants.home');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('restaurants.about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('restaurants.contact');
+})->name('contact');
+
+Route::get('/booking', function () {
+    return view('restaurants.booking');
+})->name('booking');
+
+Route::get('/menu', function () {
+    return view('restaurants.menu');
+})->name('menu');
+
+Route::get('/service', function () {
+    return view('restaurants.service');
+})->name('service');
+
+Route::get('/team', function () {
+    return view('restaurants.team');
+})->name('team');
+
+Route::get('/testimonial', function () {
+    return view('restaurants.testimonial');
+})->name('testimonial');
+
+Route::get('/admin/dasboard', function () {
+    return view('admins.index');
+})->name('home');
+
+Route::get('/auth/login', function () {
+    return view('auths.index');
+})->name('home');
