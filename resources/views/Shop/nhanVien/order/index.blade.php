f6df293384c2d958fae57268cf0ff134152b5e4f
Vinhdinh-ats <email@example.com>
2025-11-30 18:00:18 +0700

    sua trang index

--- resources/views/Shop/nhanVien/order/index.blade.php
+++ resources/views/Shop/nhanVien/order/index.blade.php
@@ -134,7 +134,6 @@
     @endforeach
 </main>
 
-<<<<<<< HEAD
 <style>
     .table-card {
         transition: transform 0.2s, box-shadow 0.2s;
@@ -196,53 +195,3 @@
 </style>
 
 @endsection
-=======
-{{-- CSS --}}
-<style>
-    .table-card {
-        background: linear-gradient(145deg, #ffffff, #f0f2f5);
-        transition: transform 0.3s, box-shadow 0.3s;
-    }
-
-    .table-card:hover {
-        transform: translateY(-5px) scale(1.02);
-        /* giảm scale tránh lệch */
-        box-shadow: 0 12px 25px rgba(0, 0, 0, 0.2);
-    }
-
-    .btn-outline-primary {
-        transition: all 0.3s;
-    }
-
-    .btn-outline-primary:hover {
-        background-color: #0d6efd;
-        color: #fff;
-    }
-
-    #flashMsg {
-        animation: fadeOut 5s forwards;
-    }
-
-    @keyframes fadeOut {
-
-        0%,
-        80% {
-            opacity: 1;
-        }
-
-        100% {
-            opacity: 0;
-        }
-    }
-
-    .table-card-header i {
-        margin-right: 5px;
-    }
-
-    .btn-lg i {
-        pointer-events: none;
-    }
-</style>
-
-@endsection
->>>>>>> origin/Trung
