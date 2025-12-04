c1c0416dc2ece8ef7a36225debb10f28a7eec889
Tran Anh Van <trananhvan@Van-Anh-Tran.lan>
2025-12-04 16:10:16 +0700

    client liên hệ

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,8 @@ Route::prefix('/')->group(function () {
         Route::get('/dich-vu', 'service')->name('service');
         Route::get('/thuc-don', 'menu')->name('menu');
         Route::get('/lien-he', 'contact')->name('contact');
+        Route::post('/lien-he', 'sendContact')->name('contact.send'); // Route GỬI form (Thêm dòng này)
+     
         Route::get('/doi-ngu', 'team')->name('team');
         Route::get('/danh-gia', 'testimonial')->name('testimonial');
     });
